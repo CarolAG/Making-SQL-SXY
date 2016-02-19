9cd87b63dc59736b7f9ae5e9128e91fda525cf36
Joshua Apstein <josh4locations@gmail.com>
2016-02-19 15:31:53 -0800

    gulpfile needs work

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ var app = express();
 app.get('/', function(req, res){
   res.send('hello world!');
 });
-
+app.get('/loggedin', function(req, res){
+  res.sendFile('/../client/index.html');
+});
 app.listen(5432, function(){
   console.log('Listening on port 5432!');
 });
