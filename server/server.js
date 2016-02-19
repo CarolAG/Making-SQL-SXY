fcf741743ae477afc7451c3404333135417f66b5
Carol Gonzalez <carolgonzalez@Carols-MacBook-Air.local>
2016-02-18 18:25:22 -0800

    fixed server added nodemon

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-var epress = require ('express');
+var express = require ('express');
 var app = express();
 
 app.get('/', function(req, res){
   res.send('hello world!');
 });
 
-app.listen(3000, function(){
-  console.log('Listeninf on port 3000!');
+app.listen(5432, function(){
+  console.log('Listening on port 5432!');
 });
